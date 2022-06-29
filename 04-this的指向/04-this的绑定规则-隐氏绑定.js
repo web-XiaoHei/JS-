dae77a196805b336aa26c6af70d5ff713392f43e
web-XiaoHei <652598147@qq.com>
2022-06-29 10:41:45 +0800

    'apply提交'

--- 04-this的指向/04-this的绑定规则-隐氏绑定.js
+++ 04-this的指向/04-this的绑定规则-隐氏绑定.js
@@ -1,10 +1,27 @@
-function foo() {
-    console.log(this);
+// 案例一
+// function foo() {
+//     console.log(this);
+// }
+
+// obj = {
+//     name: 'why',
+//     foo: foo
+// }
+// obj.foo()
+// // obj
+
+// 案例二
+var obj1 = {
+    name: 'obj1',
+    foo: function () {
+        console.log(this);
+    }
 }
 
-obj = {
-    name: 'why',
-    foo: foo
+var obj2 = {
+    name: 'obj2',
+    bar: obj1.foo
 }
-obj.foo()
-// obj
+
+obj2.bar()//obj2
+
