d96d07b089d6c86f79f459a7d82b4f63de603c3d
web-XiaoHei <652598147@qq.com>
2022-07-01 00:15:49 +0800

    '手写callapply函数'

---
+++ 07-call-apply-bind/01-call函数的实现.js
@@ -0,0 +1,24 @@
+Function.prototype.zkCall = function (thisArg, ...args) {
+    // console.log("手写call函数被调用");
+    // 在这里可以去执行调用的那个函数(foo)
+    // 问题：得可以获取到是哪一个函数执行了zkCall
+    // 1.获取需要被执行的函数
+    var fn = this
+
+    // 2.对thisArg转成对象类型(防止它传入的是非对象类型)
+    thisArg = (thisArg !== null && thisArg !== undefined) ? Object(thisArg) : window
+
+    // 3.调用需要被执行的函数 
+    thisArg.fn = fn
+    var result = thisArg.fn(...args)
+    delete thisArg.fn
+
+    // 4.将最终的结果返回出去
+    return result
+}
+
+function foo() {
+    console.log('foo函数被执行');
+}
+
+foo.zkCall({})
